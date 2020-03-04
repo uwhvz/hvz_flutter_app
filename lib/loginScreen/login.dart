1b0236ae9560eb93168a3c6b0b47019a27d7005f
Tiger Kong <tig567899@users.noreply.github.com>
2020-03-04 00:14:49 -0800

    Added Android deploy preparation

--- lib/loginScreen/login.dart
+++ lib/loginScreen/login.dart
@@ -78,6 +78,7 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin{
                 builder: (context) => HomePage()
             ));
         return;
+      case 401:
       case 403:
       case 404:
         Utilities.showErrorDialog(context, "Invalid Credentials. Please try again.");
